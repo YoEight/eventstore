315ccb59e81c1aa215eb30570f056a1aed84795d
YoEight <yo.eight@gmail.com>
2015-10-27 22:38:31 +0100

    Prepare integration testing

---
+++ tests/Tests.hs
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module : Tests
+-- Copyright : (C) 2015 Yorick Laupa
+-- License : (see the file LICENSE)
+--
+-- Maintainer : Yorick Laupa <yo.eight@gmail.com>
+-- Stability : provisional
+-- Portability : non-portable
+--
+-- Gathers all EventStore operations tests.
+--------------------------------------------------------------------------------
+module Tests where
+
+--------------------------------------------------------------------------------
---
+++ tests/integration.hs
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module : Main
+-- Copyright : (C) 2015 Yorick Laupa
+-- License : (see the file LICENSE)
+--
+-- Maintainer : Yorick Laupa <yo.eight@gmail.com>
+-- Stability : provisional
+-- Portability : non-portable
+--
+-- Main integration entry point.
+--------------------------------------------------------------------------------
+module Main where
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = return ()
