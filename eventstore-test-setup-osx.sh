356fcd19788435c17c317763279bf7205dab3d2b
YoEight <yo.eight@gmail.com>
2018-01-30 14:15:45 +0100

    Cleanup

--- eventstore-test-setup-osx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -o EventStore-OSS-MacOSX-v3.9.3.tar.gz http://download.geteventstore.com/binaries/EventStore-OSS-MacOSX-v3.9.3.tar.gz
-tar -xf EventStore-OSS-MacOSX-v3.9.3.tar.gz
-EventStore-OSS-MacOSX-v3.9.3/run-node.sh --mem-db &
--- eventstore-test-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget http://download.geteventstore.com/binaries/EventStore-OSS-Ubuntu-14.04-v3.9.3.tar.gz
-tar -xf EventStore-OSS-Ubuntu-14.04-v3.9.3.tar.gz
-EventStore-OSS-Ubuntu-14.04-v3.9.3/run-node.sh --mem-db &
