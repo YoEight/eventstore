630a480067c095b5132af8dea523722479796947
YoEight <yo.eight@gmail.com>
2019-09-24 00:16:27 +0200

    More impl. Still have to implement CloseConnection message handling.

--- Database/EventStore/Internal/Combinator/Driver.hs
+++ Database/EventStore/Internal/Combinator/Driver.hs
@@ -10,23 +10,105 @@
 module Database.EventStore.Internal.Combinator.Driver where
 
 --------------------------------------------------------------------------------
+import Control.Concurrent.Async.Lifted
+import qualified Control.Concurrent.STM.TMQueue as TMQueue
+import Data.Functor (void)
 import Data.Time (getCurrentTime)
 import Data.UUID.V4 (nextRandom)
 
 --------------------------------------------------------------------------------
 import Database.EventStore.Internal.Control
-import Database.EventStore.Internal.Connection
+import qualified Database.EventStore.Internal.Connection as ConnectionImpl
+import Database.EventStore.Internal.Discovery
 import Database.EventStore.Internal.Driver
+import           Database.EventStore.Internal.Logger
 import Database.EventStore.Internal.Stopwatch
 import Database.EventStore.Internal.Prelude
+import Database.EventStore.Internal.Types
 
 --------------------------------------------------------------------------------
 data DriverRef =
   DriverRef
-  { refStopwatch :: Stopwatch }
+  { driverRefQueue :: TMQueue.TMQueue Msg }
 
 --------------------------------------------------------------------------------
-createDriverRef :: MonadIO m => Settings -> m (Driver m)
-createDriverRef setts = do
-  builder <- liftIO $ connectionBuilder setts
-  undefined
+publishMsg :: DriverRef -> Msg -> IO ()
+publishMsg ref = atomically . TMQueue.writeTMQueue (driverRefQueue ref)
+
+--------------------------------------------------------------------------------
+newtype Ticker = Ticker { tickerStopped :: IORef Bool }
+
+--------------------------------------------------------------------------------
+newTicker :: IO Ticker
+newTicker = Ticker <$> newIORef False
+
+--------------------------------------------------------------------------------
+startTicker :: Ticker -> DriverRef -> Duration -> IO ()
+startTicker (Ticker ref) driverRef (Duration timespan) = void . fork $ go timespan
+  where
+    go n = do
+      when (n > 0) $ do
+        let waiting = min n (fromIntegral (maxBound :: Int))
+        threadDelay $ fromIntegral waiting
+        go (timespan - waiting)
+
+      publishMsg driverRef Tick
+      stopped <- readIORef ref
+      unless stopped $ go timespan
+
+--------------------------------------------------------------------------------
+newDriverRef :: IO DriverRef
+newDriverRef = DriverRef <$> TMQueue.newTMQueueIO
+
+--------------------------------------------------------------------------------
+createDriverImpl :: DriverRef
+                 -> Settings
+                 -> Discovery
+                 -> EventStore (Driver EventStore)
+createDriverImpl ref setts disc = do
+  builder <- liftIO $ ConnectionImpl.connectionBuilder setts
+  watch <- newStopwatch
+  let impl =
+        Driver { connect = connectImpl builder
+               , forceReconnect = forceReconnectImpl builder
+               , generateId = generateIdImpl
+               , discover = discoverImpl
+               , getElapsedTime = getElapsedTimeImpl watch
+               , stop = stopImpl
+               , getSettings = pure setts
+               , output = outputImpl
+               }
+
+  pure impl
+
+  where
+    connectImpl builder ept =
+      createConnectionImpl =<< ConnectionImpl.connect builder ept
+
+    forceReconnectImpl = undefined
+
+    generateIdImpl = newUUID
+
+    discoverImpl = void . fork $ do
+      tryAny (runDiscovery disc Nothing) >>= \case
+        Left e -> do
+          $logError
+            [i| Failed to resolve TCP endpoint to which to connect #{e}.|]
+          -- TODO - Implement CloseConnection message.
+          -- liftIO $ publishMsg ref (CloseConnection e)
+        Right opt ->
+          case opt of
+            Nothing ->
+              $logWarn
+                "Failed to resolve TCP endpoint to which to connect."
+            Just ept -> liftIO $ publishMsg ref (EstablishConnection ept)
+
+    getElapsedTimeImpl = stopwatchElapsed
+
+    stopImpl = undefined
+
+    outputImpl = undefined
+
+--------------------------------------------------------------------------------
+createConnectionImpl :: ConnectionImpl.Connection -> EventStore (Connection EventStore)
+createConnectionImpl = undefined
